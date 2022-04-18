efc715837feb3a92764c7c02a41f27f4431c8c67
Mike Kravtsov <mike4040@me.com>
2022-04-18 10:32:07 -0700

    protecting routes with guards

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
 
 const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app
+    .useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    )
+    .useGlobalFilters(new HttpExceptionFilter())
+    .useGlobalGuards(new ApiKeyGuard());
   await app.listen(PORT);
   console.log(`The app is running at http://www.localhost:${PORT}`);
 }
